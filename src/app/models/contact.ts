56650ea1f95ff75bbc763efdc166fcb3ac83cbc4
Angel Chavez <agchavez@unah.hn>
2021-03-14 14:30:06 -0600

    estilo contacto

---
+++ src/app/models/contact.ts
@@ -0,0 +1,10 @@
+
+
+export class ContactEmail{
+    email?:string;
+    firstname?:string;
+    lastname?:string;
+    phone?:number;
+    describe?:string;
+    
+}
--- src/app/pages/landing-page/landing-page.component.ts
+++ src/app/pages/landing-page/landing-page.component.ts
@@ -1,4 +1,6 @@
 import { Component} from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ContactEmail } from 'src/app/models/contact';
 
 @Component({
   selector: 'app-landing-page',
@@ -6,6 +8,15 @@ import { Component} from '@angular/core';
   styleUrls: ['./landing-page.component.css']
 })
 export class LandingPageComponent  {
+  email:ContactEmail = new ContactEmail();
+  constructor(){}
+
+  send(form:NgForm){
+    console.log('here');
+    if (  form.invalid ) { return; }
+    console.log(this.email.firstname);
+
+  }
 
   
 }
